fead3604b7c1df53e4a402aa3aff94e93f705133
s4lex <kelemnika1@gmail.com>
2025-10-13 23:06:56 +0700

    test_runner

--- test_runner.py
+++ test_runner.py
@@ -6,11 +6,11 @@ from django.test.utils import get_runner
 from django.conf import settings
 
 def run_tests():
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'cicd.settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
-    failures = test_runner.run_tests(['myapp.tests'])
+    failures = test_runner.run_tests(['core.tests'])
     sys.exit(bool(failures))
 
 if __name__ == '__main__':
